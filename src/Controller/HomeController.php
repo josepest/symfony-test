d9292deded199d33fcede29537bc5e01c8e5c2e1
josep <josepe@gmail.com>
2020-12-27 08:28:28 +0100

    images and assets copy

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\MessageGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(MessageGenerator $messageGenerator): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'happyMessage' => $messageGenerator->getHappyMessage()
         ]);
     }
 }
