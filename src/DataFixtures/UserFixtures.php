e62d74fd2d7cb67df0e3db9dd99e1159b3c6d281
josep <josepe@gmail.com>
2020-12-27 21:09:22 +0100

    install datafixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+
+class UserFixtures extends Fixture
+{
+    private $encoderFactory;
+
+    public function __construct(EncoderFactoryInterface $encoderFactory)
+    {
+        $this->encoderFactory = $encoderFactory;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('josepe@gmail.com');
+        $user->setPassword($this->encoderFactory->getEncoder(User::class)->encodePassword(
+            'oMPOLO010',
+            null
+        ));
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
