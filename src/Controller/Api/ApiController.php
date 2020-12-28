40b0296767191a315e5d56aea6bb57d44c342723
Josep Estudis <josepe@gmail.com>
2020-12-28 14:08:48 +0100

    api auth

---
+++ src/Controller/Api/ApiController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/api", name="api_")
+ */
+
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login", methods={"POST"})
+     */
+    public function login(Request $request)
+    {
+        $user = $this->getUser();
+
+        return $this->json([
+            'username' => $user->getUsername(),
+            'roles' => $user->getRoles(),
+        ]);
+    }
+
+    /**
+     * @Route("/list", name="list")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            '0' => 'test1',
+            '1' => 'test2',
+        ]);
+    }
+}
