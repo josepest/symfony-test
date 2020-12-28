3d95941d0d82437c728716a88ec1b05f22848b86
Josep Estudis <josepe@gmail.com>
2020-12-28 14:55:42 +0100

    install bootstrap framework

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,12 @@ import "./styles/app.scss";
 
 // start the Stimulus application
 // import "./bootstrap";
+
+const $ = require("jquery");
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require("bootstrap");
+
+$(document).ready(function () {
+  $('[data-toggle="popover"]').popover();
+});
